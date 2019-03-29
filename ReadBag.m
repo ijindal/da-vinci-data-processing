2bfe546b3adceb3bd65e703663fe8908ce272ff1
Cares Lab <parinaz.fa@gmail.com>
2019-03-29 14:49:16 -0400

    edit matlab file

--- ReadBag.m
+++ ReadBag.m
@@ -11,3 +11,6 @@ clc;close all;clear all;
 bag = rosbag('10_clutch_control_pattern4_sim.bag');
 topic1 = bag.select('Topic','/camera2/usb_cam_right/image_raw/compressed');
 data1 = readMessages(topic1);
+img_data1 = readImage(data1{1,1});
+imshow(img_data1)
+
