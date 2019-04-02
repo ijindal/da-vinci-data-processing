a117914fbabd94fc9e241ba75aebfeca3a3c9200
parinazfa <parinaz.fa@gmail.com>
2019-04-02 18:19:09 -0400

    Grid compatibility

--- ReadBag.m
+++ ReadBag.m
@@ -5,9 +5,9 @@
 clc;close all;clear all;
 %% Creat directories corresponding toeach subject for storing left and 
 % right camera images.
-rosbab_filename = '4_joystick_pattern3_sim.bag';
-subject_no = 10;
-pattern_no = 3;
+rosbab_filename = '10_clutch_control_pattern4_sim.bag';
+subject_no = 2;
+pattern_no = 4;
 [subj_dir, subj_leftimg_dir, subj_rightimg_dir] = createDirs(subject_no, pattern_no);
 %% Read rosebag files and select  image stream 
 bag = rosbag(rosbab_filename);
@@ -16,7 +16,8 @@ img_right = bag.select('Topic','/camera2/usb_cam_right/image_raw/compressed');
 data_right = readMessages(img_right);
 % Wrie images in respective folders
 for i = 1:1:length(data_right)
-    img = imresize(readImage(data_right{i,1}), [32, 32, 3]);
+    fprintf('yes its working\n')
+    img = readImage(data_right{i,1});
     baseFileName = sprintf('Image_#%d.png', i);
     fullFileName = fullfile(subj_rightimg_dir, baseFileName);
     imwrite(img, fullFileName);
@@ -28,8 +29,9 @@ data_left = readMessages(img_left);
 
 % Wrie images in respective folders
 for i = 1:1:length(data_left)
-    img = imresize(readImage(data_left{i,1}), [32, 32, 3]);
+    img = readImage(data_left{i,1});
     baseFileName = sprintf('Image_#%d.png', i);
     fullFileName = fullfile(subj_leftimg_dir, baseFileName);
     imwrite(img, fullFileName);
 end
+
