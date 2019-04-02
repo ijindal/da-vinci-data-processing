5ad25b3f26ffe4f17bfc666f5b33be732c8c95d4
Ishan Jindal <jindal2828@gmail.com>
2019-04-02 17:27:48 -0400

    added pattern functionality

--- ReadBag.m
+++ ReadBag.m
@@ -5,9 +5,10 @@
 clc;close all;clear all;
 %% Creat directories corresponding toeach subject for storing left and 
 % right camera images.
-rosbab_filename = '10_clutch_control_pattern4_sim.bag';
+rosbab_filename = '4_joystick_pattern3_sim.bag';
 subject_no = 10;
-[subj_dir, subj_leftimg_dir, subj_rightimg_dir] = createDirs(subject_no);
+pattern_no = 3;
+[subj_dir, subj_leftimg_dir, subj_rightimg_dir] = createDirs(subject_no, pattern_no);
 %% Read rosebag files and select  image stream 
 bag = rosbag(rosbab_filename);
 
@@ -15,7 +16,7 @@ img_right = bag.select('Topic','/camera2/usb_cam_right/image_raw/compressed');
 data_right = readMessages(img_right);
 % Wrie images in respective folders
 for i = 1:1:length(data_right)
-    img = readImage(data_right{i,1});
+    img = imresize(readImage(data_right{i,1}), [32, 32, 3]);
     baseFileName = sprintf('Image_#%d.png', i);
     fullFileName = fullfile(subj_rightimg_dir, baseFileName);
     imwrite(img, fullFileName);
@@ -27,7 +28,7 @@ data_left = readMessages(img_left);
 
 % Wrie images in respective folders
 for i = 1:1:length(data_left)
-    img = readImage(data_left{i,1});
+    img = imresize(readImage(data_left{i,1}), [32, 32, 3]);
     baseFileName = sprintf('Image_#%d.png', i);
     fullFileName = fullfile(subj_leftimg_dir, baseFileName);
     imwrite(img, fullFileName);
--- createDirs.m
+++ createDirs.m
@@ -1,10 +1,11 @@
-function [subj_dir, subj_leftimg_dir, subj_rightimg_dir] = createDirs(subject_no)
+function [subj_dir, subj_leftimg_dir, subj_rightimg_dir] = createDirs(subject_no, pattern_no)
     % Create subject directories for right and left camera images
     if ~exist('data/', 'dir')
         mkdir data
     end
     
     subj_name = strcat('subject_',int2str(subject_no));
+    pattern_name = strcat('pattern_',int2str(pattern_no));
     subj_dir = fullfile('./data/', subj_name);
     if ~exist(subj_dir, 'dir')
         fprintf('Directory do not exists: creating..\n')
@@ -12,9 +13,9 @@ function [subj_dir, subj_leftimg_dir, subj_rightimg_dir] = createDirs(subject_no
     else
         fprintf('Directory already exists: Do nothing \n')
     end
-
-    subj_leftimg_dir = fullfile(subj_dir, '/',strcat(subj_name, '_leftImg'));
-    subj_rightimg_dir = fullfile(subj_dir, '/',strcat(subj_name, '_rightImg'));
+    pattern_dir = fullfile(subj_dir, pattern_name );
+    subj_leftimg_dir = fullfile(pattern_dir, '/',strcat(subj_name, '_leftImg'));
+    subj_rightimg_dir = fullfile(pattern_dir, '/',strcat(subj_name, '_rightImg'));
     mkdir(subj_leftimg_dir)
     mkdir(subj_rightimg_dir)
 
